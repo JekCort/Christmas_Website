5df4701ba827920c70fb7c80882232c62089b9e5
Yevgen Kostenko <jekcort969@gmail.com>
2022-12-15 22:43:29 +0200

    commit

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 const nav = document.querySelector('nav')
 
 function changeHeader() {
-    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         nav.classList.add('change-header')
     } else {
         nav.classList.remove('change-header')
@@ -27,6 +27,7 @@ function showNaw() {
     navMenu.style.display = 'flex';
     navigation.style.height = '320px';
 }
+
 function hideNaw() {
     navMenu.style.display = 'none';
     navigation.style.height = '80px';
@@ -34,6 +35,16 @@ function hideNaw() {
 
 /* HIDE NAV WHEN GO TO SECTION */
 
+const navLinks = document.querySelectorAll('.nav__list li a');
+
+function removeNav() {
+    // Add break point using Jquery
+    if ($(window).width() < 880) {
+        navMenu.style.display = 'none';
+        navigation.style.height = '80px';
+    }
+}
+navLinks.forEach(link => link.addEventListener('click', removeNav))
 
 /* ------------- POP UP MODALS ------------- */
 
@@ -43,14 +54,14 @@ const popUp1 = document.getElementById('pop-up1');
 const img1 = document.getElementById('cup');
 const popUpImg1 = document.getElementById('pop-up-cup');
 
-img1.onclick = function (){
+img1.onclick = function () {
     popUp1.style.display = 'block';
     popUpImg1.src = this.src;
 }
 
 const span1 = document.getElementsByClassName('close1')[0];
 
-span1.onclick = function (){
+span1.onclick = function () {
     popUp1.style.display = 'none'
 }
 /* ------- 2ND POP UP ------- */
@@ -59,14 +70,14 @@ const popUp2 = document.getElementById('pop-up2');
 const img2 = document.getElementById('coat');
 const popUpImg2 = document.getElementById('pop-up-coat');
 
-img2.onclick = function (){
+img2.onclick = function () {
     popUp2.style.display = 'block';
     popUpImg2.src = this.src;
 }
 
 const span2 = document.getElementsByClassName('close2')[0];
 
-span2.onclick = function (){
+span2.onclick = function () {
     popUp2.style.display = 'none'
 }
 
@@ -76,14 +87,14 @@ const popUp3 = document.getElementById('pop-up3');
 const img3 = document.getElementById('sock');
 const popUpImg3 = document.getElementById('pop-up-socks');
 
-img3.onclick = function (){
+img3.onclick = function () {
     popUp3.style.display = 'block';
     popUpImg3.src = this.src;
 }
 
 const span3 = document.getElementsByClassName('close3')[0];
 
-span3.onclick = function (){
+span3.onclick = function () {
     popUp3.style.display = 'none'
 }
 
@@ -93,18 +104,34 @@ const popUp4 = document.getElementById('pop-up4');
 const img4 = document.getElementById('globe');
 const popUpImg4 = document.getElementById('pop-up-globe');
 
-img4.onclick = function (){
+img4.onclick = function () {
     popUp4.style.display = 'block';
     popUpImg4.src = this.src;
 }
 
 const span4 = document.getElementsByClassName('close4')[0];
 
-span4.onclick = function (){
+span4.onclick = function () {
     popUp4.style.display = 'none'
 }
 
 /* ------------- SWIPER ------------- */
+
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        880: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    }
+});
 
 /* ------------- PARTY SIGNUP FORM ------------- */
 
